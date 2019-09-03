3e59bf254be9396b41a68aec6d87525ead36da19
AdamGrzybkowski <agrzybkowski@outlook.com>
2019-09-03 11:16:18 +0200

    Fixed missing dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -2,4 +2,5 @@ object Libs {
     private const val kotlinVersion = "1.3.50"
 
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
+    const val appCompat = "androidx.appcompat:appcompat:1.0.2"
 }
