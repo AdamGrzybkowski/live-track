bf73a9c2b5d6d53057b65958da8d21b9e39a5cb7
AdamGrzybkowski <agrzybkowski@outlook.com>
2019-09-03 16:33:26 +0200

    Added main navigation graph

---
+++ presentation/src/main/java/com/adamg/livetrack/presentation/ui/tracking/TrackingFragment.kt
@@ -0,0 +1,22 @@
+package com.adamg.livetrack.presentation.ui.tracking
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.adamg.livetrack.presentation.R
+
+class TrackingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_tracking_fragmnet, container, false)
+    }
+
+
+}
