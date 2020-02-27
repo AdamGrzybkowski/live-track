ab0b5afdbacb8a45603f28b9a9a887db4c11a3eb
AdamGrzybkowski <agrzybkowski@outlook.com>
2020-02-27 22:48:18 +0100

    Updated libs

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,22 +1,22 @@
 object Libs {
-    private const val kotlinVersion = "1.3.50"
-    private const val navigationVersion = "2.1.0-rc01"
+    private const val kotlinVersion = "1.3.61"
+    private const val navigationVersion = "2.3.0-alpha02"
     private const val threetenbpVersion = "1.4.1"
-    private const val rxJavaVersion = "2.2.12"
-    private const val daggerVersion = "2.24"
-    private const val lifecycleVersion = "2.2.0-alpha05"
+    private const val rxJavaVersion = "2.2.18"
+    private const val daggerVersion = "2.26"
+    private const val lifecycleVersion = "2.2.0"
 
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
 
-    const val appCompat = "androidx.appcompat:appcompat:1.0.2"
+    const val appCompat = "androidx.appcompat:appcompat:1.2.0-alpha02"
 
     const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:$navigationVersion"
     const val navigationUi = "androidx.navigation:navigation-ui-ktx:$navigationVersion"
 
     const val lifecycle = "androidx.lifecycle:lifecycle-extensions:$lifecycleVersion"
 
-    const val material = "com.google.android.material:material:1.1.0-alpha09"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta2"
+    const val material = "com.google.android.material:material:1.2.0-alpha05"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
 
     const val dagger = "com.google.dagger:dagger:$daggerVersion"
     const val daggerProccessor = "com.google.dagger:dagger-compiler:$daggerVersion"
@@ -31,7 +31,7 @@ object Libs {
 
     const val easyPermissions = "pub.devrel:easypermissions:3.0.0"
 
-    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.0-beta-3"
+    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.2"
 
     const val threeTenBpNoTzDb = "org.threeten:threetenbp:$threetenbpVersion:no-tzdb"
     const val threeTenABP = "com.jakewharton.threetenabp:threetenabp:1.2.2"
